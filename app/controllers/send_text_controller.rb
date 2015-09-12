2309af67a60e3b2fab0f8434773aed83046bb7b1
Max Wang <maxwang051@gmail.com>
2015-09-12 16:47:47 -0400

    can send messages

---
+++ app/controllers/send_text_controller.rb
@@ -0,0 +1,29 @@
+class SendTextController < ApplicationController
+	def index
+	end
+
+	def process_sms
+		@city = params[:FromCity].capitalize
+		@state = params[:FromState]
+		render 'process_sms.xml.erb', :content_type => 'text/xml'
+	end
+
+	def send_text_message
+    number_to_send_to = params[:number_to_send_to]
+    message = params[:message]
+
+    twilio_sid = "AC0efcecadadf271dda76f44c41111e345"
+    twilio_token = "234321480deab317429df46b3c073a4b"
+    twilio_phone_number = "8327421996"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => "#{message}"
+    )
+
+    render 'index'
+  end
+end
---
+++ app/helpers/receivetext_helper.rb
@@ -0,0 +1,2 @@
+module ReceivetextHelper
+end
