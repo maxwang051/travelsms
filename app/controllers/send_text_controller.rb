29f26b493ed0e4bb43bf34179ac03a1566425fad
Max Wang <maxwang051@gmail.com>
2015-09-12 19:01:16 -0400

    trying to text forecast

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -8,7 +8,8 @@ class SendTextController < ApplicationController
 		@location = Geocoder.search(params["Body"])
 
 		ForecastIO.api_key = 'afe7d9eca604d31e23d47b7062511b0d'
-		@forecast = ForecastIO.forecast(@location[0].latitude, @location[0].longitude)
+		@forecast = ForecastIO.forecast(@location[0].latitude, @location[0].longitude, 
+			options = { lang=['en'] })
 
 		render 'process_sms.xml.erb', :content_type => 'text/xml'
 	end
