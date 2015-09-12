f1f55a76e8b64dacecefbaec30b27bc274e1839f
Max Wang <maxwang051@gmail.com>
2015-09-12 19:28:09 -0400

    trying to text forecast

--- app/controllers/receive_text_controller.rb
+++
@@ -1,8 +0,0 @@
-class ReceiveTextController < ApplicationController
-	def index
-		message_body = params["Body"]
-		from_number = params["From"]
-
-		SMSLogger.log_text_message from_number, message_body
-	end
-end
