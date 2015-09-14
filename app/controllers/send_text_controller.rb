3f3219775ae1417a00ef53ad26d240079d58410a
Max Wang <maxwang051@gmail.com>
2015-09-14 01:52:47 -0500

    works fairly well, can get weather + yelp search

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -21,23 +21,25 @@ class SendTextController < ApplicationController
 			@location = params["Body"]
 			@cords = Geocoder.search(params["Body"])[0]
 			@formatted_location = @cords.data["formatted_address"]
-			#binding.pry some magic stuff the twilio guy did to see how the data was coming in
+			#binding.pry some magic stuff the twilio guy did to see how the data was
+			#coming in
 
 			@latitude = @cords.data['geometry']['location']['lat']
-			@longitude = @cords.data['geometry']['location']['lng']	
+			@longitude = @cords.data['geometry']['location']['lng']
 
 			puts @location
 			puts @cords
 			puts @latitude
 			puts @longitude
 
-			@user = User.create(phone_number: from_number, latitude: @latitude, 
+			@user = User.create(phone_number: from_number, latitude: @latitude,
 				longitude: @longitude, location: @location)
 
-			render 'city_set.xml.erb', :content_type => 'text/xml' # displays after user types in a city
+			render 'city_set.xml.erb', :content_type => 'text/xml' # displays after
+																													# user types in a city
 		end
-		
-		ForecastIO.configure do |configuration| 
+
+		ForecastIO.configure do |configuration|
 		  configuration.api_key = 'afe7d9eca604d31e23d47b7062511b0d'
 		end
 
@@ -49,18 +51,20 @@ class SendTextController < ApplicationController
 			token: '_o_I1Le1TVVrFrrN-bj3jsAEupa6a0zy',
 			token_secret: 'cZe24B1E23oniH6QGWMUlGDKZvY'
 			})
-		
+
 
 		if params["Body"].downcase.include?('weather')
 			render 'weather.xml.erb', :content_type => 'text/xml'
 		elsif params["Body"].downcase.include?('search')
 			@response = client.search(@location, {term:params["Body"][7..-1]})
-			render 'yelp_search.xml.erb', :content_type => 'text/xml' # display if the user searches for food
+			render 'yelp_search.xml.erb', :content_type => 'text/xml' # display if the
+			# user searches for food
 		elsif params["Body"].downcase == 'done'
-			@user.delete # delete the user so the next time they use the app it will create a new location for them
-			render 'stop.xml.erb', :content_type => 'text/xml' 
+			@user.delete # delete the user so the next time they use the app it will
+			# create a new location for them
+			render 'stop.xml.erb', :content_type => 'text/xml'
 		elsif params["Body"] == @location
-		else 
+		else
 			render '404.xml.erb', :content_type => 'text/xml'
 		end
 
